847d592ec9835b75a1fdc062a4dfb67d8856a1e4
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-04-23 17:46:28 -0600

    add line commit use strict

--- mes-01/semana-01/use.js
+++ mes-01/semana-01/use.js
@@ -3,3 +3,14 @@
 "use strict";
 
 // Este codigo funciona de la manera moderna 
+
+alert("algo de código");
+// la directiva "use strict" de abajo es ignorada, tiene que estar al principio
+
+"use strict";
+
+// el modo estricto no está activado
+
+/* 'use strict'; <Shift+Enter para una nueva línea>
+//  ...tu código
+<Intro para ejecutar></Intro>   */ 
