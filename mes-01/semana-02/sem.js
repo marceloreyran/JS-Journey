e32acf7d3492d838ade59d93e282b4d405dd4a1a
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-05-14 16:37:09 -0600

    add week 2

--- mes-01/semana-02/sem.js
+++ mes-01/semana-02/sem.js
@@ -1,1 +1,7 @@
-//Esta es la semana 2 del curso de Javascript.
+//Esta es la semana 2 del curso de Javascript Journey
+
+let name = "Marcelo";
+let age = 33;
+let job = "Developer";
+
+console.log("Hola, mi nombre es: " + name + ", tengo " + age + " años " + " mi trabajo es " + job);
